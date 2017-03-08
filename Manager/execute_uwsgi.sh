ad073883898c94d40384c39b8a3b5a9b2c32b90f
root <root@ip-10-0-0-127.ap-northeast-2.compute.internal>
2017-03-08 05:21:50 +0000

    modified:   Manager/execute_uwsgi.sh

--- Manager/execute_uwsgi.sh
+++ Manager/execute_uwsgi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup uwsgi --http-socket :8080 --plugin python --wsgi-file /var/lib/gncloud-store/Manager/__init__.py --logto /var/log/gncloud-store/manager.log --processes 4 --threads 2 --callable app &
+nohup uwsgi --http-socket :8081 --plugin python --wsgi-file /var/lib/gncloud-store/Manager/__init__.py --logto /var/log/gncloud-store/manager.log --processes 4 --threads 2 --callable app &

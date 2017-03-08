50b7c60bb142cabb71a5b30059307ef07eb075e6
root <root@ip-10-0-0-127.ap-northeast-2.compute.internal>
2017-03-08 04:52:01 +0000

    new file:   Manager/execute_uwsgi.sh

---
+++ Manager/execute_uwsgi.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup uwsgi --http-socket :8080 --plugin python --wsgi-file /var/lib/gncloud/Manager/__init__.py --logto /var/log/gncloud/manager.log --processes 4 --threads 2 --callable app &
